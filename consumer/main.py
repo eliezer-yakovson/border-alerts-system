1d7ad3c758cbbcf124d7a1f1cbbb05d94e289675
Eliezer Yakovson <eliezeryakovson@gmail.com>
2026-02-19 11:08:16 +0200

    Incubating the consumer, pulling alerts from redis and sending them to mongo, arranged in order of urgency.

---
+++ consumer/main.py
@@ -0,0 +1,24 @@
+import redis
+import pymongo
+import json
+from datetime import datetime
+
+def connect_to_redis():
+    return redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
+
+def connect_to_mongo():
+    client = pymongo.MongoClient('mongodb://mongo:27017')
+    return client.border_alerts
+
+def process_alerts():
+    redis_conn = connect_to_redis()
+    db = connect_to_mongo()
+    while True:
+        alert_data = redis_conn.lpop('urgent_queue') or redis_conn.lpop('normal_queue')
+        if alert_data:
+            alert = json.loads(alert_data)
+            alert['insertion_time'] = datetime.utcnow().isoformat()
+            db.alerts.insert_one(alert)
+
+if __name__ == '__main__':
+    process_alerts()
